48949e201b64f0b796e325f3bf2b495ec4463dfe
maxpleaner <maxpleaner@gmail.com>
2018-01-20 14:56:08 -0800

    get working with sinatra auth in a box

--- server/config.ru
+++ server/config.ru
@@ -1,2 +1,2 @@
 require './server.rb'
-Server.run!
+run Server
--- server/server.rb
+++ server/server.rb
@@ -4,15 +4,42 @@ require 'active_support/all'
 require 'em-websocket'
 require 'sinatra/base'
 require 'data_mapper'
+require 'byebug'
 
 require_relative './lib/game.rb'
 require_relative './lib/websockets.rb'
 require_relative './lib/database.rb'
+require_relative './lib/auth_in_a_box.rb'
 
 Thread.new { EM.run { Websockets.start! } }
 
+
 class Server < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
-    "hello world"
+    login_required
+    "you're in"
+  end
+
+  get '/login' do
+    render_login    # or render your own equivalent!
+  end
+
+  post '/login' do
+    login
+  end
+
+  get '/signup' do
+    render_signup   # or render your own equivalent!
+  end
+
+  post '/signup' do
+    signup
+  end
+
+  get '/logout' do
+    logout
   end
 end
